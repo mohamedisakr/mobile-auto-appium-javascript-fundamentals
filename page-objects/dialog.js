21bd084b7c39cac8382044fef847b2e2e2909264
mohamedisakr <mohamedisakr@gmail.com>
2020-12-19 08:51:49 -0800

    finding elements

--- page-objects/dialog.js
+++ page-objects/dialog.js
@@ -8,6 +8,14 @@ class Dialog {
       "//android.widget.EditText[@resource-id='io.appium.android.apis:id/username_edit']"
     );
   }
+
+  get $everyMonday() {
+    return $("//android.widget.CheckedTextView[@index='0']");
+  }
+
+  get $$everyday() {
+    return $$("//android.widget.CheckedTextView");
+  }
 }
 
 module.exports = new Dialog();
